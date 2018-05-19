34eaee813c1baec9a4a39bebac5c7d09a4ce275a
okp1007 <okp.engineer1007@gmail.com>
2018-05-19 11:12:44 +0900

    done

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,63 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // シンバルの音源ファイルを指定
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    
+    // シンバル用のプレイヤーインスタンス生成
+    var cymbalPlayer = AVAudioPlayer()
+    
+    @IBAction func cymbal(_ sender: Any) {
+        do {
+            // シンバル用のプレイヤーに、音源ファイル名を指定
+            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+            
+            // シンバルの音源再生
+            cymbalPlayer.play()
+        } catch {
+            print("シンバルで、エラーが発生しました！")
+        }
+    }
+    
+    // ギターの音源ファイルを指定
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    
+    // ギター用のプレイヤーインスタンス生成
+    var guitarPlayer = AVAudioPlayer()
+    
+    @IBAction func guitar(_ sender: Any) {
+        do {
+            // ギター用のプレイヤーに、音源ファイル名を指定
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            
+            // ギターの音源再生
+            guitarPlayer.play()
+        } catch {
+            print("ギターで、エラーが発生しました！")
+        }
+    }
+    
+    // バックミュージックの音源ファイルを指定
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    
+    // バックミュージック用のプレイヤーインスタンスを生成
+    var backmusicPlayer = AVAudioPlayer()
+    
+    @IBAction func play(_ sender: Any) {
+        do {
+            // バックミュージック用のプレイヤーに、音源ファイル名を指定
+            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint:nil)
+            
+            // リピート設定
+            backmusicPlayer.numberOfLoops = -1
+            backmusicPlayer.play()
+        } catch {
+            print("エラーが発生しました")
+        }
+    }
+    @IBAction func stop(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
 }
 
